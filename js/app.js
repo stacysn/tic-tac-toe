d65ff01f5406744aecfebb7890fadd226f12ad54
YOUR_GITHUB_USERNAME <stacysuen@Stacy-Stace.local>
2017-07-04 16:32:53 -0700

    added the columns and rows

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 // wait for the DOM to finish loading
+console.log("Helloooooo");
+var $divs = $('div')
+
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+})
 
-});
+console.log($divs);
