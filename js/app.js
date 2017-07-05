e465e48b128d4707632770da29e4f4508e4ac55e
YOUR_GITHUB_USERNAME <stacysuen@Stacy-Stace.attlocal.net>
2017-07-04 22:53:00 -0700

    mostly pseudocode; still in progress

--- js/app.js
+++ js/app.js
@@ -5,8 +5,50 @@ console.log("Helloooooo");
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  var $rows = $('.row');
-  $('.row').on("click", function() {alert ("Hello There!");
-  });
+  let gameArr = [];
+  let turns = ['X', 'O', 'X','O', 'X', 'O', 'X', 'O', 'X']
+  let $rows = $('.row');
 
-})
+  for (let i = 0; i < turns.length; i++){  //9 because of 9 boxes
+    if ($('.row').on("click", function(){
+      if (i % 2 === 0){
+        alert ("Player 2's turn")
+      }
+      else {
+        alert ("Player 1's turn")
+      }
+      console.log("testing");
+    })
+  })
+});
+
+
+//psuedocode
+// for every turn (click) on box
+// --> i = turns[i]
+// ---> append to array of which box specifically
+// -->(if a box is not taken/if not in the array, then proceed)
+// --> if any of the possibilities of X or O wins, or even a cats game, then alert
+// reset.
+//
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < turns.length; i++){  //9 because of 9 boxes
+//     if ($('.row').on("click", function()){
+//       alert ("Hello There!");
+//       alert(turns[i]);
+//     };
+// };
+
+//for every turn (click) on box
+//i = turns[i]
+//append to array
+
+// $('.row').on("click", function() {alert ("Hello There!")}
