f193c750ce850e5305b215185861aec8d6923203
YOUR_GITHUB_USERNAME <stacysuen@Stacy-Stace.attlocal.net>
2017-07-05 00:01:44 -0700

    added failed funct to change color

--- js/app.js
+++ js/app.js
@@ -6,40 +6,45 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
   let gameArr = [];
-  let turns = ['X', 'O', 'X','O', 'X', 'O', 'X', 'O', 'X']
-  let $rows = $('.row');
+  let turns = ['X','O','X','O','X','O','X','O','X']
+  // let $rows = $('.row');
 
-  for (let i = 0; i < turns.length; i++){  //9 because of 9 boxes
-    if ($('.row').on("click", function(){
-      if (i % 2 === 0){
-        alert ("Player 2's turn")
-      }
-      else {
-        alert ("Player 1's turn")
-      }
-      console.log("testing");
-    })
-  })
+  for (let i = 0; i < turns.length; i++){
+    // if ($('#one').on("click", function(){alert("hi!")})){
+    //   console.log("testing");
+    // }
+    //
+    //   else if (i % 2 === 0){
+    //
+    //   }
+    if(i % 2 === 0){
+      ($('.row').click(function(){ //trying to change that one box that is clicked;works only when specific id is called/clicked
+        $(this).css('color', 'red');   //changing color of box/number once clicked
+      }))
+    }
+ }
+
+
+
 });
 
+// document.getElementById("button").reset("Reset")
 
-//psuedocode
+
+//*****************************psuedocode*******************************//
 // for every turn (click) on box
 // --> i = turns[i]
 // ---> append to array of which box specifically
 // -->(if a box is not taken/if not in the array, then proceed)
 // --> if any of the possibilities of X or O wins, or even a cats game, then alert
 // reset.
-//
 
 
 
 
 
 
-
-
-
+//***************Self Notes on ideas/past code ****************************//
 // for (let i = 0; i < turns.length; i++){  //9 because of 9 boxes
 //     if ($('.row').on("click", function()){
 //       alert ("Hello There!");
@@ -51,4 +56,5 @@ $(document).ready(function() {
 //i = turns[i]
 //append to array
 
-// $('.row').on("click", function() {alert ("Hello There!")}
+
+// $('.row').on("click", function() {alert ("Hello There!")})
