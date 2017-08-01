882ff6aa680794896690d3a3b38359e5f0d4e8d7
stacysn <stacysuen5@gmail.com>
2017-07-31 17:34:09 -0700

    got x's and o's to render onto page

--- js/app.js
+++ js/app.js
@@ -1,60 +1,47 @@
-// wait for the DOM to finish loading
-
-console.log("Helloooooo");
-
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  let gameArr = [];
-  let turns = ['X','O','X','O','X','O','X','O','X']
+  let $boxes = $('.box');
+  let turn = "X"; //first move is X
   // let $rows = $('.row');
+  //first turn is X, then switch to O
+  function whoGoes(){
+    if (turn === "X"){
+      turn = "O";
+    } else if (turn === "O"){
+        turn = "X"
+      };
+  };
 
-  for (let i = 0; i < turns.length; i++){
-    // if ($('#one').on("click", function(){alert("hi!")})){
-    //   console.log("testing");
+  function spotTaken(){
+    // if ($('.box').text("X"{
+    //   console.log(("WHAT"));
+    //   // ($('.box').text(turn));
+    // } else {
+    //   console.log("you can't move here!")
     // }
-    //
-    //   else if (i % 2 === 0){
-    //
-    //   }
-    if(i % 2 === 0){
-      ($('.row').click(function(){ //trying to change that one box that is clicked;works only when specific id is called/clicked
-        $(this).css('color', 'red');   //changing color of box/number once clicked
-      }))
+  }
+
+
+
+  $('.box').click(function(){
+    $(this).text(turn);
+    whoGoes();
+    spotTaken();
+    // console.log($(this));
+  })
+
+
+  //reset Game
+  function resetGame(){
+    turn = "X";
+    if ($('.box').text("X") || $(box).text("O") || $(box).text("")){
+      $('.box').text(" ");
     }
- }
+  }
 
 
-
+  $('#reset').click(function(){
+    resetGame();
+  });
 });
-
-// document.getElementById("button").reset("Reset")
-
-
-//*****************************psuedocode*******************************//
-// for every turn (click) on box
-// --> i = turns[i]
-// ---> append to array of which box specifically
-// -->(if a box is not taken/if not in the array, then proceed)
-// --> if any of the possibilities of X or O wins, or even a cats game, then alert
-// reset.
-
-
-
-
-
-
-//***************Self Notes on ideas/past code ****************************//
-// for (let i = 0; i < turns.length; i++){  //9 because of 9 boxes
-//     if ($('.row').on("click", function()){
-//       alert ("Hello There!");
-//       alert(turns[i]);
-//     };
-// };
-
-//for every turn (click) on box
-//i = turns[i]
-//append to array
-
-
-// $('.row').on("click", function() {alert ("Hello There!")})
